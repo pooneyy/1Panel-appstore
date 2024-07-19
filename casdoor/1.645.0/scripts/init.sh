85f6e564b36c9f078e27086447924f448d329742
萌森 <qyg2297248353@163.com>
2024-07-19 13:16:10 +0800

    发布应用 Casdoor
    身份和访问管理(IAM)/单点登录(SSO)平台
    
    Signed-off-by: 萌森 <qyg2297248353@163.com>

---
+++ casdoor/1.645.0/scripts/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  source .env
+
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+
+  if [ "$DB_TYPE" == "mysql" ]; then
+    echo "dataSourceName=\"$DB_USER:$DB_PASSWD@tcp($DB_HOSTNAME:$DB_PORT)/\"" >> .env
+  fi
+
+  if [ "$DB_TYPE" == "postgresql" ]; then
+    echo "dataSourceName=\"user=$DB_USER password=$DB_PASSWD host=$DB_HOSTNAME port=$DB_PORT sslmode=disable dbname=$DB_NAME\"" >> .env
+  fi
+
+  if [ -z "$REDIS_PASSWORD" ]; then
+    echo "redisEndpoint=\"$REDIS_HOSTNAME:$REDIS_PORT,$REDIS_DBINDEX,$REDIS_PASSWORD\"" >> .env
+  else
+    echo "redisEndpoint=\"$REDIS_HOSTNAME:$REDIS_PORT,$REDIS_DBINDEX\"" >> .env
+  fi
+
+  echo "Check Finish."
+
+else
+  echo "Error: .env file not found."
+fi
