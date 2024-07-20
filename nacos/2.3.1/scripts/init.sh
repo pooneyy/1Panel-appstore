cd065fae28bda7172d5353f87fc7b39b2806c9bf
萌森 <qyg2297248353@163.com>
2024-07-20 13:43:02 +0800

    维护 Nacos
    Signed-off-by: 萌森 <qyg2297248353@163.com>

--- nacos/2.3.1/scripts/init.sh
+++ nacos/2.3.1/scripts/init.sh
@@ -3,6 +3,10 @@
 if [ -f .env ]; then
   source .env
 
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+
   if command -v mysql &> /dev/null; then
     if [[ -f ./init/mysql-schema.sql ]]; then
       mysql -u"$MYSQL_SERVICE_USER" -p"$MYSQL_SERVICE_PASSWORD" -h"$MYSQL_SERVICE_HOST" -P"$MYSQL_SERVICE_PORT" --protocol=TCP "$MYSQL_SERVICE_DB_NAME" < ./init/mysql-schema.sql
--- nacos/2.3.1/scripts/upgrade.sh
+++ nacos/2.3.1/scripts/upgrade.sh
@@ -3,6 +3,10 @@
 if [ -f .env ]; then
   source .env
 
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+
   echo "Check Finish."
 
 else
