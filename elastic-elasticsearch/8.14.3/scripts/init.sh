542da645ba62196dfe1a2818c406e6d16eee2e79
萌森 <qyg2297248353@163.com>
2024-07-23 13:07:50 +0800

    调试 elastic
    Signed-off-by: 萌森 <qyg2297248353@163.com>

--- elastic-elasticsearch/8.14.3/scripts/init.sh
+++ elastic-elasticsearch/8.14.3/scripts/init.sh
@@ -8,15 +8,7 @@ if [ -f .env ]; then
   sed -i '/^ENV_FILE=/d' .env
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
 
-  mkdir -p "$ELASTICSEARCH_ROOT_PATH"
-  mkdir -p "$ELASTICSEARCH_DATA_PATH/data"
-  mkdir -p "$ELASTICSEARCH_DATA_PATH/backup"
-  mkdir -p "$ELASTICSEARCH_DATA_PATH/plugins"
-
-  chown -R 1000:1000 "$ELASTICSEARCH_ROOT_PATH"
-  chown -R 1000:1000 "$ELASTICSEARCH_ROOT_PATH/data"
-  chown -R 1000:1000 "$ELASTICSEARCH_ROOT_PATH/backup"
-  chown -R 1000:1000 "$ELASTICSEARCH_ROOT_PATH/plugins"
+  chmod -R 777 $ELASTICSEARCH_ROOT_PATH
 
   echo "Check Finish."
 else
