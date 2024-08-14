bbd4c5ca920d2cc696ddcde07acc6f00a80e5f50
萌森 <qyg2297248353@163.com>
2024-08-14 11:25:03 +0800

    发布应用: 爱影CMS
    Signed-off-by: 萌森 <qyg2297248353@163.com>

--- iycms/3.3.44/scripts/upgrade.sh
+++ iycms/3.3.44/scripts/upgrade.sh
@@ -8,19 +8,6 @@ if [ -f .env ]; then
   sed -i '/^ENV_FILE=/d' .env
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
 
-  if [ ! -d "$IYCMS_ROOT_PATH/config" ]; then
-    mkdir -p "$IYCMS_ROOT_PATH/config"
-  fi
-
-  if [ -d "$IYCMS_ROOT_PATH/config/config.conf" ]; then
-    rm -r "$IYCMS_ROOT_PATH/config/config.conf"
-    touch "$IYCMS_ROOT_PATH/config/config.conf"
-  elif [ ! -f "$IYCMS_ROOT_PATH/config/config.conf" ]; then
-    cp ./config/config.conf "$IYCMS_ROOT_PATH/config/config.conf"
-  else
-    echo "config.conf cp error."
-  fi
-
   echo "Check Finish."
 
 else
