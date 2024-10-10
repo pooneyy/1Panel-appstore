1b5518394045ac01dc88d43601ce357e1f65c000
Meng Sen <qyg2297248353@gmail.com>
2024-10-10 11:55:48 +0800

    脚本修复
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- stream-rec-frontend/0.7.1/scripts/init.sh
+++ stream-rec-frontend/0.7.1/scripts/init.sh
@@ -3,6 +3,11 @@
 if [ -f .env ]; then
   source .env
 
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  sed -i '/^ENV_FILE=/d' .env
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+
   echo "Check Finish."
 
 else
