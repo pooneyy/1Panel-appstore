db3dbc33d41bf88af75fa2b0851bdd6d492ee32e
Meng Sen <qyg2297248353@gmail.com>
2024-10-30 17:04:16 +0800

    发布应用 Synapse
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- synapse/1.118.0/scripts/init.sh
+++ synapse/1.118.0/scripts/init.sh
@@ -8,18 +8,6 @@ if [ -f .env ]; then
   sed -i '/^ENV_FILE=/d' .env
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
 
-  # setup-2 generate key
-  mkdir -p "${CURRENT_DIR}/data"
-  docker run -it --rm \
-        -v "${CURRENT_DIR}/data":/data \
-        -e SYNAPSE_SERVER_NAME="${DOMAIN_NAME}" \
-        -e SYNAPSE_REPORT_STATS=no \
-        -e TZ=Asia/Shanghai \
-        matrixdotorg/synapse:latest generate
-
-  # setup-3 check permission
-  chmod -R 777 "${CURRENT_DIR}/data"
-
   echo "Check Finish."
 
 else
