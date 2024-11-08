0ef5a1060cbeea8ceed651372bb9bcbaa4516997
Meng Sen <qyg2297248353@gmail.com>
2024-11-08 17:49:26 +0800

    发布 MoviePilot V2
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

---
+++ movie-pilot/2.0.2/scripts/upgrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  source .env
+
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  sed -i '/^ENV_FILE=/d' .env
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+
+  echo "Check Finish."
+else
+  echo "Error: .env file not found."
+fi
