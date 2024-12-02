d373e5031d7af56f91711e92dbf1c076b3b6b73a
Meng Sen <qyg2297248353@gmail.com>
2024-12-02 17:37:49 +0800

    发布应用：Topiam
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- topiam-ce/2.0.0/scripts/init.sh
+++ topiam-ce/2.0.0/scripts/init.sh
@@ -10,6 +10,11 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
+  # setup-2 create directories
+  mkdir -p "$TOPIAM_CE_ROOT_PATH"
+  mkdir -p "$TOPIAM_CE_ROOT_PATH/logs"
+  chmod -R 777 "$TOPIAM_CE_ROOT_PATH"
+
   echo "Check Finish."
 
 else
