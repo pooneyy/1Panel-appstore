54081deed5179d0690290496400a403570d93b30
Meng Sen <qyg2297248353@gmail.com>
2024-12-02 15:51:55 +0800

    发布应用： 泡泡广场 All-in-One TOPIAM 身份管理平台
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

---
+++ paopao-ce-all/0.6.0/scripts/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  source .env
+
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  sed -i '/^ENV_FILE=/d' .env
+  sed -i '/^GLOBAL_ENV_FILE=/d' .env
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
+
+  # setup-2 copy config file
+  mkdir -p "$PAOPAO_CE_ROOT_PATH"
+  mkdir -p "$PAOPAO_CE_ROOT_PATH/config"
+  cp ./conf/config.yaml "$PAOPAO_CE_ROOT_PATH/config/config.yaml"
+
+  echo "Check Finish."
+
+else
+  echo "Error: .env file not found."
+fi
