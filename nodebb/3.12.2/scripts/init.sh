c428fc54fbbfebf2ecd2462735fe7aec0d8e494d
Meng Sen <qyg2297248353@gmail.com>
2025-01-13 13:37:59 +0800

    修复 nodedb 环境变量被覆盖
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- nodebb/3.12.2/scripts/init.sh
+++ nodebb/3.12.2/scripts/init.sh
@@ -17,8 +17,32 @@ if [ -f .env ]; then
   mkdir -p "$NODEBB_ROOT_PATH/uploads"
   mkdir -p "$NODEBB_ROOT_PATH/config"
 
-  cp ./conf/setup.json "$NODEBB_ROOT_PATH/setup.json"
+  # setup-3 初始化参数配置
+  if [ -z "$NODEBB_DB_NAME" ]; then
+    NODEBB_DB_NAME="nodebb"
+  fi
 
+  if [ -z "$NODEBB_DB_USER" ]; then
+    NODEBB_DB_USER=""
+  fi
+
+  if [ -z "$NODEBB_DB_PASSWORD" ]; then
+    NODEBB_DB_PASSWORD=""
+  fi
+
+  SETUP_JSON_PATH="$NODEBB_ROOT_PATH/setup.json"
+  cp ./conf/setup.json "$SETUP_JSON_PATH"
+  sed -i "
+    s|\$NODEBB_DB_HOST|$NODEBB_DB_HOST|g;
+    s|\$NODEBB_DB_PORT|$NODEBB_DB_PORT|g;
+    s|\$NODEBB_DB_NAME|$NODEBB_DB_NAME|g;
+    s|\$NODEBB_DB_USER|$NODEBB_DB_USER|g;
+    s|\$NODEBB_DB_PASSWORD|$NODEBB_DB_PASSWORD|g;
+    s|\$NODEBB_DB|$NODEBB_DB|g
+  " "$SETUP_JSON_PATH"
+
+
+  # setup-4 update dir permissions
   chown -R 1001:1001 "$NODEBB_ROOT_PATH"
   chown -R 1001:1001 "$NODEBB_ROOT_PATH/node_modules"
   chown -R 1001:1001 "$NODEBB_ROOT_PATH/build"
--- nodebb/3.12.2/scripts/upgrade.sh
+++ nodebb/3.12.2/scripts/upgrade.sh
@@ -18,9 +18,19 @@ if [ -f .env ]; then
   mkdir -p "$NODEBB_ROOT_PATH/config"
 
   if [ ! -f "$NODEBB_ROOT_PATH/setup.json" ]; then
-    cp ./conf/setup.json "$NODEBB_ROOT_PATH/setup.json"
+    SETUP_JSON_PATH="$NODEBB_ROOT_PATH/setup.json"
+    cp ./conf/setup.json "$SETUP_JSON_PATH"
+    sed -i "
+      s|\$NODEBB_DB_HOST|$NODEBB_DB_HOST|g;
+      s|\$NODEBB_DB_PORT|$NODEBB_DB_PORT|g;
+      s|\$NODEBB_DB_NAME|$NODEBB_DB_NAME|g;
+      s|\$NODEBB_DB_USER|$NODEBB_DB_USER|g;
+      s|\$NODEBB_DB_PASSWORD|$NODEBB_DB_PASSWORD|g;
+      s|\$NODEBB_DB|$NODEBB_DB|g
+    " "$SETUP_JSON_PATH"
   fi
 
+  # setup-3 update dir permissions
   chown -R 1001:1001 "$NODEBB_ROOT_PATH"
   chown -R 1001:1001 "$NODEBB_ROOT_PATH/node_modules"
   chown -R 1001:1001 "$NODEBB_ROOT_PATH/build"
