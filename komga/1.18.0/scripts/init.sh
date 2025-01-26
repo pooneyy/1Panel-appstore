3e8ed8cbb3cc0235cddfa5594c8392713c6a3327
Meng Sen <qyg2297248353@gmail.com>
2025-01-26 12:20:13 +0800

    安装权限修复
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- komga/1.18.0/scripts/init.sh
+++ komga/1.18.0/scripts/init.sh
@@ -12,7 +12,13 @@ if [ -f .env ]; then
 
   # setup-2 copy config file
   mkdir -p "$KOMGA_ROOT_PATH"
+  mkdir -p "$KOMGA_ROOT_PATH/config"
+  mkdir -p "$KOMGA_ROOT_PATH/data"
+
   chown -R 1000:1000 "$KOMGA_ROOT_PATH"
+  chown -R 1000:1000 "$KOMGA_ROOT_PATH/config"
+  chown -R 1000:1000 "$KOMGA_ROOT_PATH/data"
+
 
   echo "Check Finish."
 
