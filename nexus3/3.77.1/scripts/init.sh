389c6757e39430e73b16efc93553c1f36386aac4
Meng Sen <qyg2297248353@gmail.com>
2025-02-21 16:46:25 +0800

    发布应用 Nexus3
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- nexus3/3.77.1/scripts/init.sh
+++ nexus3/3.77.1/scripts/init.sh
@@ -10,6 +10,12 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
+  mkdir -p "$NEXUS3_ROOT_PATH"
+  mkdir -p "$NEXUS3_ROOT_PATH/data"
+
+  chown -R 200:0 "$NEXUS3_ROOT_PATH"
+  chown -R 200:0 "$NEXUS3_ROOT_PATH/data"
+
   echo "Check Finish."
 
 else
