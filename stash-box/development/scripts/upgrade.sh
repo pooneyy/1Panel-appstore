5395a1a0b475e869f5f947e6c654d8fa6a1e69a9
Meng Sen <qyg2297248353@gmail.com>
2025-03-21 14:01:18 +0800

    发布 Stash Box
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- stash-box/development/scripts/upgrade.sh
+++ stash-box/development/scripts/upgrade.sh
@@ -10,13 +10,6 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
-  # setup-2 create stash-box-config.yml
-  mkdir -p "$STASH_BOX_ROOT_PATH"
-  mkdir -p "$STASH_BOX_ROOT_PATH/data"
-  if [ ! -f "$STASH_BOX_ROOT_PATH/data/stash-box-config.yml" ]; then
-    echo "database: ${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_NAME}" > "$STASH_BOX_ROOT_PATH/data/stash-box-config.yml"
-  fi
-
   echo "Check Finish."
 
 else
