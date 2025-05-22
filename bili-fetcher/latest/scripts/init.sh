522b82c23fabda6ea45dcec560ab83fc96caf524
Meng Sen <qyg2297248353@gmail.com>
2025-05-22 17:11:52 +0800

    发布 BiliFetcher
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- bili-fetcher/latest/scripts/init.sh
+++ bili-fetcher/latest/scripts/init.sh
@@ -10,6 +10,11 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
+  if [ ! -d $BILI_FETCHER_ROOT_PATH/config ]; then
+    mkdir -p $BILI_FETCHER_ROOT_PATH/config
+  fi
+  cp -rn ./config/* $BILI_FETCHER_ROOT_PATH/config/
+
   echo "Check Finish."
 
 else
